9ea562400ecb9eeca4e567ecb93c2cb67fb0c038
eszych@gmail.com <eszych@gmail.com>
2017-12-15 18:28:48 +0100

    Ubuntu AZPKG download

--- admin/141_Create_Ubuntu_Linux_16.04.3-LTS_Image.ps1
+++ admin/141_Create_Ubuntu_Linux_16.04.3-LTS_Image.ps1
@@ -20,10 +20,24 @@ Add-AzsVMImage `
     -osDiskLocalPath $OSdiskPath `
     -CreateGalleryItem $false 
 
-$RG = New-AzureRmResourceGroup -Name tenantartifacts -Location local
+if (!(Get-AzureRmResourceGroup -ResourceGroupName tenantartifacts -ErrorAction SilentlyContinue))
+{
+    $RG = New-AzureRmResourceGroup -Name tenantartifacts -Location local
+} else {
+    $RG = Get-AzureRmResourceGroup -ResourceGroupName tenantartifacts -ErrorAction SilentlyContinue
+}
+
 $StorageAccount = New-AzureRmStorageAccount -Name tenantartifacts -Type Standard_LRS -ResourceGroupName $RG.Resourcegroupname -Location local
 
-$GalleryContainer = New-AzureStoragseContainer -Name gallery -Permission Blob -Context $StorageAccount.Context
-$GalleryContainer | Set-AzureStorageBlobContent -File "D:\AzureStack_Installer\Canonical.UbuntuServer.1.0.0.azpkg" -Verbose
+if (!(Get-AzureStorageContainer -name gallery -Context $StorageAccount.Context -ErrorAction SilentlyContinue))
+{
+    $GalleryContainer = New-AzureStorageContainer -Name gallery -Permission Blob -Context $StorageAccount.Context
+} else {
+    $GalleryContainer = Get-AzureStorageContainer -name gallery -Context $StorageAccount.Context -ErrorAction SilentlyContinue
+}
+
+Invoke-WebRequest "https://raw.githubusercontent.com/Microsoft/PartsUnlimitedMRP/master/deploy/azurestack/instances/ubuntu_server_1604_base/Canonical.UbuntuServer.1.0.0.azpkg" -OutFile "D:\Canonical.UbuntuServer.1.0.0.azpkg"
+
+$GalleryContainer | Set-AzureStorageBlobContent -File "D:\Canonical.UbuntuServer.1.0.0.azpkg" -Verbose
 $GalleryItemURI = (Get-AzureStorageBlob -Context $StorageAccount.Context -Blob "Canonical.UbuntuServer.1.0.0.azpkg" -Container 'gallery').ICloudBlob.uri.AbsoluteUri
 Add-AzsGalleryItem -GalleryItemUri $GalleryItemURI -Verbose 
